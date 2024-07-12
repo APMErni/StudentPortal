49fc7a439ba1fdc5e426aa07b22cbdf4425a6cf9
alquea <alquea.macatangay@betterask.erni>
2024-07-12 17:58:35 +0800

    added crud files

---
+++ StudentPortal/StudentPortal.Web/Migrations/20240712054922_Migration after changing year level to program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentPortal.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrationafterchangingyearleveltoprogram : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "YearLevel",
+                table: "Students",
+                newName: "Program");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Program",
+                table: "Students",
+                newName: "YearLevel");
+        }
+    }
+}
