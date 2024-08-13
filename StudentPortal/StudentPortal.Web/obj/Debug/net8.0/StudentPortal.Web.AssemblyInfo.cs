5d538985b6966434181fa76de9d24abb6111e4fd
alquea <alquea.macatangay@betterask.erni>
2024-08-13 08:40:11 +0800

    Final changes

--- StudentPortal/StudentPortal.Web/obj/Debug/net8.0/StudentPortal.Web.AssemblyInfo.cs
+++ StudentPortal/StudentPortal.Web/obj/Debug/net8.0/StudentPortal.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a9f762cb5a2693b9cd832d35c73ad444b0fa6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba63d8308e3130fc4b307b8323d680aa56985d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
